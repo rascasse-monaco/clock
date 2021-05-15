9ab0df8a7eaf3c8f3e26001e899346a1a9204867
rascasse-monaco <mktwkhr@gmail.com>
2021-05-15 19:04:40 +0900

    コードの整理

--- clock.js
+++ clock.js
@@ -1,26 +1,32 @@
 'use strict';
 
-setInterval(() => {
-  
+/**
+ * @param {object} now 現在時刻のミリ秒 
+ * @returns {string} 文字列化して、１桁の場合は10の位に0を入れて２桁にする。
+ */
+function toStrAndDubDigt(now) {
+  return now.toString().padStart(2, '0');
+}
+
+setInterval(() => { // 表示を１秒毎に更新
   const now = new Date();
-  const timeArrey = {
-    hour: now.getHours().toString().padStart(2, '0'),
-    min: now.getMinutes().toString().padStart(2, '0'),
-    sec: now.getSeconds().toString().padStart(2, '0')
+  const time = { // 時計の文字列を連想配列に定義
+    hour: toStrAndDubDigt(now.getHours()),
+    min: toStrAndDubDigt(now.getMinutes()),
+    sec: toStrAndDubDigt(now.getSeconds())
   };
-  const calendarArrey = {
+  const calendar = { // カレンダーの文字列を連想配列に定義
     year: now.getFullYear(),
-    month: (now.getMonth() + 1).toString().padStart(2, '0'),
-    date: now.getDate().toString().padStart(2, '0'),
+    month: toStrAndDubDigt((now.getMonth() + 1)),
+    date: toStrAndDubDigt(now.getDate()),
     dayText: [ 'Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.' ]
   };
-
+  // 時計表示部分
   const timeElement = document.getElementById('time');
   timeElement.innerHTML = 
-    `${timeArrey.hour}:${timeArrey.min}:${timeArrey.sec}`;
-
+    `${time.hour}:${time.min}:${time.sec}`;
+  // カレンダー表示部分
   const calendarElement = document.getElementById('calendar');
   calendarElement.innerHTML =
-    `${calendarArrey.year}.${calendarArrey.month}.${calendarArrey.date}.${calendarArrey.dayText[now.getDay()]}`;
-
+    `${calendar.year}.${calendar.month}.${calendar.date}.${calendar.dayText[now.getDay()]}`;
 }, 1000);
